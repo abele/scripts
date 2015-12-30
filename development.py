9a47b1a174308453fff2604f229353e66b820c52
Jānis Ābele <janis.abele@gmail.com>
2015-12-30 11:43:23 +0200

    Fix virtualenv activation and deactivation

--- development.py
+++ development.py
@@ -46,41 +46,40 @@ def scratch(args, stdin=None):
     xonsh.dirstack.cd([scratch_dir])
     print('New \'{}\' dir ready for grinding (⌐■_■)'.format(new_dir))
 
-_ORIG_PATH = None
-_ORIG_PROMPT = None
 
 def workon(args, stdin=None):
     """Activate Python virtualenv."""
-    env = builtins.__xonsh_env__
-    if args:
-        venv_name = args[0]
-    else:
-        venv_name = 'venv'
-
-    _ORIG_PATH = list(env['PATH'])
-    _ORIG_PROMPT = env['PROMPT']
+    venv_name = first(args, 'venv')
     venv_path = os.path.join(os.getcwd(), venv_name)
 
     if not os.path.exists(venv_path):
-        raise OSError('No such virtualenv: %s' % venv_path)
+        raise OSError('Faild to activate venv: no such venv{}'.format(venv_path))
 
-    env['PATH'] = [os.path.join(venv_path, 'bin')]  + _ORIG_PATH
+    env = builtins.__xonsh_env__
+
+    if  'VIRTUAL_ENV' in env:
+        deactivate(None, None)
+
+    path_orig = list(env['PATH'])
+    prompt_orig = env['PROMPT']
+
     env['VIRTUAL_ENV'] = venv_path
-    env['PROMPT'] = '({}) {}'.format(venv_name, _ORIG_PROMPT)
+    env['PROMPT'] = '({}) {}'.format(venv_name, prompt_orig)
+    env['PATH'] = [os.path.join(venv_path, 'bin')]  + path_orig
+
+    workon._path_orig = path_orig
+    workon._prompt_orig = prompt_orig
 
 
 def deactivate(args, stdin=None):
     """Deactivate Python virtualenv."""
     env = builtins.__xonsh_env__
-    del env['VIRTUAL_ENV']
-    env['PATH'] = list(_ORIG_PATH)
-    env['PROMPT'] = _ORIG_PROMPT
-
-
-def z(args, stdin=None):
-    _fasd_ret = _run_cmd('fasd -d ' + args[0])
-    _fasd_ret = _fasd_ret.splitlines()[0]
-    xonsh.dirstack.cd([_fasd_ret])
+    try:
+        del env['VIRTUAL_ENV']
+        env['PATH'] = workon._path_orig
+        env['PROMPT'] = workon._prompt_orig
+    except KeyError:
+        raise OSError('Failed to deactivate virtualenv.')
 
 
 def setup():
@@ -91,7 +90,6 @@ def setup():
     builtins.aliases['workon'] = workon
     builtins.aliases['work'] = workon
     builtins.aliases['deactivate'] = deactivate
-    builtins.aliases['z'] = z
 
 
 def _run_cmd(cmd):
@@ -101,3 +99,7 @@ def _run_cmd(cmd):
         stderr=subprocess.STDOUT,
         shell=True,
     )
+
+
+def first(iterable, default=None):
+    return next(iter(iterable or []), default)
